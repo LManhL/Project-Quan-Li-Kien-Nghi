ad0d0e1f16ae58cb86f00a004ed6cf823975d4a2
vy285 <vy.lt280502@gmail.com>
2022-11-28 10:39:58 +0700

    giao dien user

--- ProjectQuanLiKienNghi/src/main/java/com/example/projectquanlikiennghi/controller/controllerLogin/AfterLoginUser.java
+++ ProjectQuanLiKienNghi/src/main/java/com/example/projectquanlikiennghi/controller/controllerLogin/AfterLoginUser.java
@@ -10,8 +10,25 @@ import java.io.IOException;
 public class AfterLoginUser {
     @FXML
     private Button buttonLogout;
+    @FXML
+    private BorderPane Bpane_phu;
+
     public void userLogout(ActionEvent event) throws IOException {
         Main m = new Main();
         m.changeScene("login.fxml");
     }
+
+
+    @FXML
+    void userInformation(ActionEvent event) {
+        Parent p = new FXMLLoader().load(getClass().getResource("information_user.fxml"));
+        Bpane_phu.setCenter(p);
+    }
+
+    @FXML
+    void userList(ActionEvent event) {
+        Parent p = new FXMLLoader().load(getClass().getResource("danh_sach_ykien_user.fxml"));
+        Bpane_phu.setCenter(p);
+    }
+
 }
