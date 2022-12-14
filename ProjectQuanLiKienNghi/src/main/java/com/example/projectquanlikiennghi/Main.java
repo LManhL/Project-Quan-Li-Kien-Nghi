a3516b0c1b1f5c1834fd1d4273fcf92770ae6f4a
Hoang Pham <huyhoang2522@gmail.com>
2022-12-14 21:41:17 +0700

    M10 world cup 2022

--- ProjectQuanLiKienNghi/src/main/java/com/example/projectquanlikiennghi/Main.java
+++ ProjectQuanLiKienNghi/src/main/java/com/example/projectquanlikiennghi/Main.java
@@ -27,7 +27,9 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+
         launch(args);
+        System.out.println("M10 World Cup 2022");
     }
 
 }
